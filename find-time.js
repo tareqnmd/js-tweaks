b89518aec5fd1b3b189a09da1ce561a18028569d
Mohammad Tareq <94968552+tareqatoffice@users.noreply.github.com>
2023-02-03 10:19:32 +0600

    Create find-time.js

---
+++ find-time.js
@@ -0,0 +1,7 @@
+const now = new Date();
+const countDownDate = new Date("Jan 29, 2023 22:00:00").getTime();
+const distance = countDownDate - now;
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
