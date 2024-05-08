0d2f0b9db9c9ab9013038fdbde57f40b6db20349
Mohammad Tareq <94968552+tareqatoffice@users.noreply.github.com>
2024-05-09 01:04:35 +0600

    Update elm-in-viewport.js

--- elm-in-viewport.js
+++ elm-in-viewport.js
@@ -9,16 +9,32 @@ function isElementInViewport(element) {
 	);
 }
 
-function isElementPartiallyVisible(element) {
-	const rect = element.getBoundingClientRect();
-	const viewportWidth =
-		window.innerWidth || document.documentElement.clientWidth;
-	const viewportHeight =
-		window.innerHeight || document.documentElement.clientHeight;
+function getElementVisibility(el) {
+    var rect = el.getBoundingClientRect();
+    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
-	const isVisible =
-		(rect.top < viewportHeight && rect.bottom > 0) ||
-		(rect.left < viewportWidth && rect.right > 0);
-
-	return isVisible;
+    // Check if the element is fully within the viewport
+    if (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= viewportHeight &&
+        rect.right <= viewportWidth
+    ) {
+        return 'fully';
+    }
+    // Check if the element is partially within the viewport
+    else if (
+        rect.bottom > 0 &&
+        rect.right > 0 &&
+        rect.top < viewportHeight &&
+        rect.left < viewportWidth
+    ) {
+        return 'partially';
+    }
+    // Otherwise, the element is completely outside the viewport
+    else {
+        return 'outside';
+    }
 }
+
